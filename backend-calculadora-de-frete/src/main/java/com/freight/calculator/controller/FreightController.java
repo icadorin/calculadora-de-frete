2c3397ef5db95799d5fe36de5783583e6265231e
origin <cadorinisrael@gmail.com>
2025-04-18 05:41:51 -0300

    Fix adapt controllers to work with railway env vars

--- backend-calculadora-de-frete/src/main/java/com/freight/calculator/controller/FreightController.java
+++ backend-calculadora-de-frete/src/main/java/com/freight/calculator/controller/FreightController.java
@@ -3,28 +3,33 @@ package com.freight.calculator.controller;
 import com.freight.calculator.dto.FreightCalculationRequest;
 import com.freight.calculator.dto.FreightCalculationResponse;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.freight.calculator.service.FreightCalculationService;
 
 @RestController
-@RequestMapping("${APP_API_BASE_PATH}${APP_API_FRETE_PATH}")
 public class FreightController {
 
-    private final FreightCalculationService freightCalculationService;
+    private final FreightCalculationService service;
+    private final String basePath;
 
-    public FreightController(FreightCalculationService freightCalculationService) {
-        this.freightCalculationService = freightCalculationService;
+    public FreightController(
+            FreightCalculationService service,
+            @Value("${APP_API_BASE_PATH:/api}") String basePath,
+            @Value("${APP_API_FRETE_PATH:/frete}") String fretePath
+    ) {
+        this.service = service;
+        this.basePath = basePath + fretePath;
     }
 
     @PostMapping("/calcular")
     public ResponseEntity<FreightCalculationResponse> calculateFreight(
-            @RequestBody @Valid FreightCalculationRequest request) {
-
-        FreightCalculationResponse response = freightCalculationService.calculatorFreight(request);
+            @RequestBody @Valid FreightCalculationRequest request
+    ) {
+        FreightCalculationResponse response = service.calculatorFreight(request);
         return ResponseEntity.ok(response);
     }
 }
